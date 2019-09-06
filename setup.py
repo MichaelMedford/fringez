ae20501f54f5b694d0ad6482de5de908f76fd2b8
Michael Medford <MichaelMedford@berkeley.edu>
2019-09-05 20:31:00 -0700

    rewrite setup.py following sampleproject template

--- setup.py
+++ setup.py
@@ -1,53 +1,54 @@
 #! /usr/bin/env python
 """
 __setup__.py
+Inspired by: https://github.com/pypa/sampleproject
 """
 
 DESCRIPTION = "Removing atmospheric fringes from ZTF i-band images."
-LONG_DESCRIPTION = """Removing atmospheric fringes from ZTF i-band images."""
 
 DISTNAME = 'fringez'
 AUTHOR = 'Michael Medford'
-MAINTAINER = 'Michael Medford'
-MAINTAINER_EMAIL = 'michaelmedford@berkeley.edu'
+AUTHOR_EMAIL = 'michaelmedford@berkeley.edu'
 URL = 'https://github.com/MichaelMedford/fringez'
 LICENSE = 'MIT'
 VERSION = open('VERSION').readline().strip()
 DOWNLOAD_URL = 'https://github.com/MichaelMedford/fringez/tarball/%s' % VERSION
 
-try:
-    from setuptools import setup, find_packages
-    _has_setuptools = True
-except ImportError:
-    from distutils.core import setup
-_has_setuptools = False
+from setuptools import setup, find_packages
+from os import path
 
-if __name__ == "__main__":
+here = path.abspath(path.dirname(__file__))
 
-    if _has_setuptools:
-        packages = find_packages()
-    else:
-        # This should be updated if new submodules are added
-        packages = ['fringez']
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
 
-    setup(name=DISTNAME,
-          author=AUTHOR,
-          author_email=MAINTAINER_EMAIL,
-          maintainer=MAINTAINER,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          long_description=LONG_DESCRIPTION,
-          license=LICENSE,
-          url=URL,
-          version=VERSION,
-          download_url=DOWNLOAD_URL,
-          packages=packages,
-          classifiers=[
-              'Intended Audience :: Science/Research',
-              'Programming Language :: Python :: 3.6',
-              'License :: OSI Approved :: MIT License',
-              'Topic :: Scientific/Engineering :: Astronomy',
-              'Operating System :: POSIX',
-              'Operating System :: Unix',
-              'Operating System :: MacOS'],
-          )
+setup(name=DISTNAME,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+      license=LICENSE,
+      url=URL,
+      version=VERSION,
+      download_url=DOWNLOAD_URL,
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+      python_requires='>=3.5',
+      install_requires=['astropy',
+                        'joblib',
+                        'numpy',
+                        'matplotlib',
+                        'scikit-learn=0.19.1'],
+      scripts=['bin/fringez-generate',
+               'bin/fringez-clean'],
+      classifiers=['Intended Audience :: Science/Research',
+                   'Programming Language :: Python :: 3.6',
+                   'License :: OSI Approved :: MIT License',
+                   'Topic :: Scientific/Engineering :: Astronomy',
+                   'Operating System :: POSIX',
+                   'Operating System :: Unix',
+                   'Operating System :: MacOS'],
+      project_urls={'Models': 'https://portal.nersc.gov/project/ztf/iband'}
+)
